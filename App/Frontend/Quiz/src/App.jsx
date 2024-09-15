d968b3bdb60680e7eb2d9f510d37cec9fb205f4f
Omkar Kathile <omkarkathile007@gmail.com>
2024-09-15 12:42:54 +0530

    Add frontend Quiz feature

---
+++ App/Frontend/Quiz/src/App.jsx
@@ -0,0 +1,9 @@
+import QuizApp from "./components/QuizApp";
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+      <QuizApp/>
+    </h1>
+  )
+}
