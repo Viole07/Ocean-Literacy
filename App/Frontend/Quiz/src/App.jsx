8c36fc9fdcb0c3a45d0e3fdd865ebd6656e5f38c
Omkar Kathile <157679976+OmkarKathile007@users.noreply.github.com>
2024-09-16 23:09:42 +0530

    Update App.jsx

--- App/Frontend/Quiz/src/App.jsx
+++ App/Frontend/Quiz/src/App.jsx
@@ -2,8 +2,9 @@ import QuizApp from "./components/QuizApp";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
+    <>
       <QuizApp/>
-    </h1>
+    </>
+
   )
 }
